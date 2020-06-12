2b152873e20ed6330610e442f45277f6ece77492
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-12 14:18:39 -0400

    added initial files

---
+++ app.js
@@ -0,0 +1,43 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 800 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+  };
+
+  return {
+    logData: function() {
+      return data;
+    }
+  };
+})();
+
+// UI Controller
+const UICtrl = (function() {})();
+
+// App Controller
+const AppCtrl = (function(ItemCtrl, UICtrl) {
+  return {
+    init: function() {
+      console.log('Initializing App...');
+    }
+  };
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+AppCtrl.init();
